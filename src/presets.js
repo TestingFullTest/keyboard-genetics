46688fb2215ca97781cb41aa7d8e8753fcb13fb8
TestingFullTest <137568290+TestingFullTest@users.noreply.github.com>
2024-09-23 11:17:34 +0200

    Update presets.js - Added Swedish Layout

--- src/presets.js
+++ src/presets.js
@@ -63,6 +63,16 @@ const presets = {
      A O E U I D H T N S _ \\n
       ; q j k x b m w v z
       : Q J K X B M W V Z
+  `,
+  Swedish: `
+  \§ 1 2 3 4 5 6 7 8 9 0 + ´
+   ½ ! " # ¤ % & / ( ) = ? ˙
+     q w e r t y u i o p å ¨ \\
+     Q W E R T Y U I O P Å ^ |
+     a s d f g h j k l ö ä ' \\n
+     A S D F G H J K L Ö Ä * \\n
+      z x c v b n m , . -
+      Z X C V B N M ; : _
   `
 };
 
